dac0f349bda1148b31ee5eec0d07040b969bffce
TedEngineer <tedvu184@gmail.com>
2023-02-08 20:20:46 +1100

    Set up basic hooks file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import UseStateHooks from "./hooks/UseStateHooks";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+  },
+  {
+    path: "/useState",
   },
 ]);
 
