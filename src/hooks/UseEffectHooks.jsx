c4538c8bf1f3020220b82061980ab56e88803e3d
TedEngineer <tedvu184@gmail.com>
2023-02-11 15:22:58 +1100

    Add sample code for useEffect hooks

--- src/hooks/UseEffectHooks.jsx
+++ src/hooks/UseEffectHooks.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+
 function UseEffectHooks() {
-  return <h1>Use Effect Hooks</h1>;
+  const [count, setCount] = useState(0);
+  // 3 types of useEffect with dependency array
+  useEffect(() => {
+    // this line runs when application renders initially
+    setCount(100);
+  }, []);
+
+  useEffect(() => {
+    alert("I will appear after every state change and intially");
+
+    // Questions how many times I will be rendered initially ;)
+    // Answer: 3 times - initially, first assignment of count  = 0 and after we set count by using the
+    // above hooks
+  });
+
+  useEffect(() => {
+    alert("I will appear after every count state change");
+  }, [count]);
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      {" "}
+      <h1>{count}</h1>
+      <button
+        style={{ margin: "1rem" }}
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        style={{ margin: "1rem" }}
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
 export default UseEffectHooks;
