39788d06f1b0be9f2e9481d7e838bf3ac1bf442f
TedEngineer <tedvu184@gmail.com>
2023-02-11 15:58:51 +1100

    Adding sample code of useContextHooks

--- src/hooks/UseContextHooks.jsx
+++ src/hooks/UseContextHooks.jsx
@@ -1,11 +1,42 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 function UseContextHooks() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>Use Context Hooks</h1>
+      <ParentComponent />
     </div>
   );
 }
 
 export default UseContextHooks;
+
+const UserContext = createContext();
+function ParentComponent() {
+  const [user, setUser] = useState("Ted Vu");
+  return (
+    <UserContext.Provider value={user}>
+      <div>
+        <h1>Parent component </h1>
+        <ChildComponent1 />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+function ChildComponent1() {
+  return (
+    <div>
+      <ChildComponent2 />
+    </div>
+  );
+}
+
+function ChildComponent2() {
+  const user = useContext(UserContext);
+  return (
+    <div>
+      <h1>Child component 2</h1>
+      <h1>Hello from {user}</h1>
+    </div>
+  );
+}
